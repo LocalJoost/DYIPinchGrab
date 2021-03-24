655647b975f09f2060c1ec50974e4580576b3053
Joost van Schaik <joostvanschaik@outlook.com>
2021-03-24 12:13:10 +0100

    Initial code checkin

---
+++ Assets/Application/Scripts/DyiHandManipulation.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using MRKTExtensions.Gesture;
+using UnityEngine;
+
+namespace DyiPinchGrab
+{
+    public class DyiHandManipulation : MonoBehaviour
+    {
+        [SerializeField]
+        private TrackedHandJoint trackedHandJoint = TrackedHandJoint.IndexMiddleJoint;
+
+        [SerializeField]
+        private float grabDistance = 0.1f;
+
+        [SerializeField]
+        private Handedness trackedHand = Handedness.Both;
+
+        [SerializeField]
+        private bool trackPinch = true;
+
+        [SerializeField]
+        private bool trackGrab = true;
+
+        private IMixedRealityHandJointService handJointService;
+
+        private IMixedRealityHandJointService HandJointService =>
+            handJointService ??
+            (handJointService = CoreServices.GetInputSystemDataProvider<IMixedRealityHandJointService>());
+
+        private MixedRealityPose? previousLeftHandPose;
+
+        private MixedRealityPose? previousRightHandPose;
+
+        private void Update()
+        {
+            var leftHandPose = GetHandPose(Handedness.Left, previousLeftHandPose != null);
+            var rightHandPose = GetHandPose(Handedness.Right, previousRightHandPose != null);
+            {
+                var jointTransform = HandJointService.RequestJointTransform(trackedHandJoint, trackedHand);
+                if (rightHandPose != null && previousRightHandPose != null)
+                {
+                    if (leftHandPose != null && previousLeftHandPose != null)
+                    {
+                        // fight! pick the closest one
+                        var isRightCloser = Vector3.Distance(rightHandPose.Value.Position, jointTransform.position) <
+                                            Vector3.Distance(leftHandPose.Value.Position, jointTransform.position);
+
+                        ProcessPoseChange(
+                            isRightCloser ? previousRightHandPose : previousLeftHandPose,
+                            isRightCloser ? rightHandPose : leftHandPose);
+                    }
+                    else
+                    {
+                        ProcessPoseChange(previousRightHandPose, rightHandPose);
+                    }
+                }
+                else if (leftHandPose != null && previousLeftHandPose != null)
+                {
+                    ProcessPoseChange(previousLeftHandPose, leftHandPose);
+                }
+            }
+            previousLeftHandPose = leftHandPose;
+            previousRightHandPose = rightHandPose;
+        }
+
+        private MixedRealityPose? GetHandPose(Handedness hand, bool hasBeenGrabbed)
+        {
+            if ((trackedHand & hand) == hand)
+            {
+                if (HandJointService.IsHandTracked(hand) &&
+                    ((GestureUtils.IsPinching(hand) && trackPinch) ||
+                     (GestureUtils.IsGrabbing(hand) && trackGrab)))
+                {
+                    var jointTransform = HandJointService.RequestJointTransform(trackedHandJoint, hand);
+                    var palmTransForm = HandJointService.RequestJointTransform(TrackedHandJoint.Palm, hand);
+                    
+                    if(hasBeenGrabbed || 
+                       Vector3.Distance(gameObject.transform.position, jointTransform.position) <= grabDistance)
+                    {
+                        return new MixedRealityPose(jointTransform.position, palmTransForm.rotation);
+                    }
+                }
+            }
+
+            return null;
+        }
+        
+        private void ProcessPoseChange(MixedRealityPose? previousPose, MixedRealityPose? currentPose)
+        {
+            var delta = currentPose.Value.Position - previousPose.Value.Position;
+            var deltaRotation = Quaternion.FromToRotation(previousPose.Value.Forward, currentPose.Value.Forward);
+            gameObject.transform.position += delta;
+            gameObject.transform.rotation *= deltaRotation;
+        }
+    }
+}
---
+++ Assets/MRTKExtensions/Gesture/GestureUtils.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.MixedReality.Toolkit.Utilities;
+
+namespace MRKTExtensions.Gesture
+{
+    public static class GestureUtils
+    {
+        private const float PinchThreshold = 0.7f;
+        private const float GrabThreshold = 0.4f;
+        
+        public static bool IsPinching(Handedness trackedHand)
+        {
+            return HandPoseUtils.CalculateIndexPinch(trackedHand) > PinchThreshold;
+        }
+
+        public static bool IsGrabbing(Handedness trackedHand)
+        {
+            return !IsPinching(trackedHand) &&
+                   HandPoseUtils.MiddleFingerCurl(trackedHand) > GrabThreshold &&
+                   HandPoseUtils.RingFingerCurl(trackedHand) > GrabThreshold &&
+                   HandPoseUtils.PinkyFingerCurl(trackedHand) > GrabThreshold &&
+                   HandPoseUtils.ThumbFingerCurl(trackedHand) > GrabThreshold;
+        }
+    }
+}
